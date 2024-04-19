4c396ee82340c735082b1b3ebb05be0e22af2503
Miguel <159616816+MiguelEIbrahim@users.noreply.github.com>
2024-04-19 18:40:08 +0200

    Add files via upload

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# myproject/urls.py
 from django.urls import path
+from voting.views import SignUpView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    ...
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
---
+++ myproject/myproject/views.py
@@ -0,0 +1,9 @@
+# voting/views.py
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import SignUpForm
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
