8f00a2e2084dd0b7c71bf2f384ccf9943408b3b8
Miguel Ibrahim <159616816+MiguelEIbrahim@users.noreply.github.com>
2024-05-28 11:37:45 +0200

    changing to wamp server see if things can work out from there

---
+++ front-end/Private/Vote/fetch_candiates.php
@@ -0,0 +1,38 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "roote";
+$password = "";
+$dbname = "uplet";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to get all candidates
+$sql = "SELECT Name, Elder, Num_Times_Poked, Evil_Plan FROM yondora";
+$result = $conn->query($sql);
+
+$candidates = [];
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $candidates[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+// Shuffle the candidates
+shuffle($candidates);
+
+// Return the shuffled list as JSON
+header('Content-Type: application/json');
+echo json_encode($candidates);
+?>
